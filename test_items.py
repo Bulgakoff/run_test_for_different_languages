4bb64fabedeaa6c677e18450a0f8339d8e2adba7
Alex <hlbu@yandex.ru>
2021-10-11 14:01:59 +0300

    last_1

--- test_items.py
+++ test_items.py
@@ -6,7 +6,6 @@ def test_is_button_french(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     time.sleep(5)
-    browser.find_element_by_css_selector("#login_link")
     # butt_len = len(browser.find_element_by_css_selector("button[value='Ajouter au panier']").text)
     # butt_len = len(browser.find_elements_by_css_selector("button[value='Ajouter au panier']"))# list
     # butt_len = len(browser.find_element_by_xpath(('//button[contains(@value,"Ajouter au panier")]')).text)
