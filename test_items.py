39511ca30bd47215406d2e83d235ddb5f74e9989
Alex <hlbu@yandex.ru>
2021-10-13 14:07:10 +0300

    last comment

--- test_items.py
+++ test_items.py
@@ -1,15 +1,11 @@
 import time
 
 
-
-def test_is_button_french(browser):
+def test_is_button_fr_es(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     time.sleep(5)
-    # butt_len = len(browser.find_element_by_css_selector("button[value='Ajouter au panier']").text)
-    # butt_len = len(browser.find_elements_by_css_selector("button[value='Ajouter au panier']"))# list
-    # butt_len = len(browser.find_element_by_xpath(('//button[contains(@value,"Ajouter au panier")]')).text)
-    butt= browser.find_elements_by_xpath(('//button[contains(@value,"Ajouter au panier")]'))
-    print(f'=========================={butt}')
+    button = browser.find_elements_by_css_selector("button.btn.btn-lg.btn-primary.btn-add-to-basket")
+    print(f'=========================={button}')
 
-    assert butt, 'NO BUTTON!'
+    assert button, 'NO BUTTON!'
